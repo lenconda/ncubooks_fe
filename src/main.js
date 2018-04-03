612be8ce852b2a433f3b89b516030d430a53c54e
lenconda <1053464288@qq.com>
2018-04-03 12:50:14 +0800

    添加Vuex，并完成配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 // 初始化CSS，一定要在最上面引入
 import './assets/normalize.less'
 //引入Flexible
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  appName: 'NCUBooks'
+}
+
+const mutations = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
