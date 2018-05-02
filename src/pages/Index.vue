37678876b8518ad4cbe7dc772f751af1f41c2151
lenconda <1053464288@qq.com>
2018-05-02 14:52:59 +0800

    [framework]搭建好Index页面的骨架

---
+++ src/pages/Index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="index">
+    <div class="container">
+      <router-view></router-view>
+    </div>
+    <footer class="tabbar">
+      <router-link class="item" :to="{path: '/index/explore'}">
+        <div class="wrapper">
+          <div class="icon">
+            <img src="../assets/img/explore_active.png" alt="explore_active" width="100%">
+          </div>
+          <span class="text">发现</span>
+        </div>
+      </router-link>
+      <router-link class="item" :to="{path: '/index/mine'}">
+        <div class="wrapper">
+          <div class="icon">
+            <img src="../assets/img/mine.png" alt="mine" width="100%">
+          </div>
+          <span class="text">我的</span>
+        </div>
+      </router-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style lang="less">
+.index {
+  height: 100%;
+  padding-bottom: 50px;
+  .container {
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+  .tabbar {
+    height: 50px;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    background-color: #fff;
+    box-shadow: 0 -2px 2px 0 rgba(0, 0, 0, .09);
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    .item {
+      display: flex;
+      flex-grow: 1;
+      height: 100%;
+      justify-content: center;
+      align-items: center;
+      .wrapper {
+        width: auto;
+        .icon {
+          width: 25px;
+          height: 25px;
+          margin-bottom: 2.5px;
+        }
+        .text {
+          font-size: 10px;
+          color: #b1bdc6;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Test from '@/test/Test'
 
+import Index from '@/pages/Index'
+import Index_Explore from '@/pages/index/Explore'
+import Index_Mine from '@/pages/index/Mine'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'portal',
+    },
+    {
+      path: '/index',
       name: 'index',
-      redirect: '/test'/*以后要移除这个跳转*/
+      component: Index,
+      children: [
+        {
+          path: 'explore',
+          name: 'explore',
+          component: Index_Explore
+        },
+        {
+          path: 'mine',
+          name: 'mine',
+          component: Index_Mine
+        }
+      ]
     },
     {
       path: '/test',
